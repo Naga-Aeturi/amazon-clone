2a1a9f6f347f52d1c6b9af7ed2511490c8d92df3
Naga-Aeturi <aeturipradeep@gmail.com>
2023-07-26 22:16:06 +0530

    added home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
-    <Header></Header>
-    
+    <div className='app'>
+      <Header></Header>
+      <Home></Home>
+    </div>
   );
 }
 
